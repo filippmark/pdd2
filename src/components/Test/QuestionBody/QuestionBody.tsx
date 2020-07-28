d5ba2b34159c9b239f85e9242ef7c674c1e50e9c
Filip <lenovofm12@outlook.com>
2020-07-28 17:33:55 +0300

    fix

--- src/components/Test/QuestionBody/QuestionBody.tsx
+++ src/components/Test/QuestionBody/QuestionBody.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { FormGroup, Button } from "reactstrap";
 import "./QuestionBody.css";
